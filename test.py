c7d0ddc57bff9972c59207c02e5f38b029310bc6
Krishna <hikrish1@gmail.com>
2022-09-10 10:01:38 +0530

    modified

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print('Hello world')
+print('Whats your name \t')
+name = input()
+print('hello', name)
+for c in name:
+    print(c.upper(),'\t')
+    #print(\t)
+age=input('Enter your age\t')
+print(name, 'Is ', age, 'year old')
