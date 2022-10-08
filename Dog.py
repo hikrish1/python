31c287c84aee13be4345ef1e775beb2bb26dc2d4
Krishna <hikrish1@gmail.com>
2022-10-08 22:05:36 +0530

    change

---
+++ Dog.py
@@ -0,0 +1,7 @@
+class Dog():
+    species = "mammal"
+    def __init__(self, breed, name, spots):
+        self.breed = breed
+        self.name = name
+        self.spots = spots
+my_dog = Dog('Lab','Tommy',False)
