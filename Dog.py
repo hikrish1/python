46448c800e06842d958b96f1ea03fe2e158ce0b3
Krishna <hikrish1@gmail.com>
2022-10-09 09:42:47 +0530

    new file

--- Dog.py
+++ Dog.py
@@ -1,7 +1,9 @@
 class Dog():
-    species = "mammal"
+    species = "mammal
     def __init__(self, breed, name, spots):
         self.breed = breed
         self.name = name
         self.spots = spots
-my_dog = Dog('Lab','Tommy',False)
+
+
+my_dog = Dog('Lab', 'Tommy', False)
